3a08142eb717d6fe3dff106874724c510e16592b
Ivan Xu <oopsstafan@gmail.com>
2022-01-14 16:05:01 +1100

    v1

--- README.md
+++ README.md
@@ -1,70 +1,23 @@
-# Getting Started with Create React App
+## Getting Started
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+1. Make sure you have installed all the required packages and libraries using "npm install" before you start.
+2. This application is created by "create-react-app" so be sure to start app by running "npm start"
 
-## Available Scripts
 
-In the project directory, you can run:
+## Introduction
+This is a small weather info application created by React.js. Datasource was priorly given, all required weather info is shown and here is a forecast visualization on the webpage.
 
-### `npm start`
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+## Assumptions
+1. All information is shown on one single page (I assume there is no need to have more than 1 page for required info)
+2. I convert the given json file to js file for easy importing into the module. The js file is in weatherinfo folder.
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
 
-### `npm test`
+## Resources
+1. React.js (staging tool is 'create-react-app')
+2. antd(UI component library for responsive and delicate design)
+3. echarts and echarts-for-react(These are for data visualization)
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
 
-### `npm run build`
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+## Thank you for your opportunity!
--- src/App.js
+++ src/App.js
@@ -1,23 +1,131 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Card, Descriptions, Popover, Button } from 'antd'
+import ReactECharts from 'echarts-for-react';
+import React from 'react';
+import '../node_modules/antd/dist/antd.css'
+import weatherInfo from './weatherInfo/weatherInfo'
 function App() {
+  React.useEffect(() => {
+    /*
+      In the real case, we are going to get AJAX data from server side
+      when component firstly mounted. The data should be in json format.
+      We can diretly handle json as js object and save it in the state for further
+      reuse.
+    */
+
+    //  show data format in console
+    console.log(weatherInfo.query.results.channel.item.forecast)
+  }, [])
+  let { location, item, units, wind, atmosphere, astronomy } = weatherInfo.query.results.channel
+  //get forecast info as array
+  let forecastInfo = item.forecast
+  //get the first seven needed forecast info
+  forecastInfo.length = 7
+  //get needed date info as new array
+  const dateArr = forecastInfo.map((item)=>{
+    return item.date
+  })
+  //get needed day info as new array
+  const dayArr = forecastInfo.map((item)=>{
+    return item.day
+  })
+  //get needed high temperature info as new array
+  const hiArr = forecastInfo.map((item)=>{
+    return item.high
+  })
+  //get needed low temperature info as new array
+  const lowArr = forecastInfo.map((item)=>{
+    return item.low
+  })
+  //get weather condition info as new array
+  const textArr = forecastInfo.map((item)=>{
+    return item.text
+  })
+  //set options for chart
+  const options = {
+    title: {
+      //title of the chart
+      text: 'Temperature Change in the Coming Week'
+    },
+    tooltip: {
+      trigger: 'axis',
+      // triggerOn: 'click',
+      formatter: function(arg){
+        //set up day and condition var to further store day and condition info
+        let day, condition
+        //get needed day and weather condition info(when the dataIndex equals to index of the weatherinfo array)
+        dayArr.forEach((item, index)=>{
+          if (arg[0].dataIndex === index) day = item
+        })
+        textArr.forEach((item, index)=>{
+          if (arg[0].dataIndex === index) condition = item
+        })
+        //show needed day, date, high temp, low temp, and weather condition info
+        return `Day: ${day} <br/> Date: ${arg[0].axisValue} <br/> High Temp: ${arg[0].data} <br/> Low Temp: ${arg[1].data} <br/> Condition: ${condition}`
+      }
+    },
+    legend: {},
+    xAxis: {
+      type: 'category',
+      boundaryGap: true,
+      //xAxis is date
+      data: dateArr
+    },
+    yAxis: {
+      type: 'value',
+      axisLabel: {
+        //y axis is value with unit °F'
+        formatter: '{value} °F'
+      }
+    },
+    //here are two lines, high temp and low temp
+    series: [
+      {
+        name: 'High Temp',
+        type: 'line',
+        data: hiArr,
+      },
+      {
+        name: 'Low Temp',
+        type: 'line',
+        data: lowArr,
+      }
+      
+    ]
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ width: '60%', margin: '0 auto' }}>
+      <Card title="New York's Weather Test" >
+        <Descriptions title="Current Conditions">
+          <Descriptions.Item label="Location">{location.city}, {location.country}, {location.region}</Descriptions.Item>
+          <Descriptions.Item label="Weather Description">{item.condition.text}</Descriptions.Item>
+          <Descriptions.Item label="Temperature">{item.condition.temp} {units.temperature}</Descriptions.Item>
+          <Descriptions.Item label="High Temperature">{item.forecast[0].high} {units.temperature}</Descriptions.Item>
+          <Descriptions.Item label="Low Temperature">{item.forecast[0].low} {units.temperature}</Descriptions.Item>
+        </Descriptions>
+        <br />
+        <Popover content={wind.speed + units.speed} trigger="hover">
+          <Button>Wind Speed</Button>
+        </Popover>&nbsp;
+        <Popover content={atmosphere.humidity} trigger="hover">
+          <Button>Humidity</Button>
+        </Popover>&nbsp;
+        <Popover content={atmosphere.pressure + units.pressure} trigger="hover">
+          <Button>Pressure</Button>
+        </Popover>&nbsp;
+        <Popover content={astronomy.sunrise} trigger="hover">
+          <Button>Sunrise Time</Button>
+        </Popover>&nbsp;
+        <Popover content={astronomy.sunset} trigger="hover">
+          <Button>Sunset Time</Button>
+        </Popover>&nbsp;
+        <br/>
+        <br/>
+        <ReactECharts style={{height: '500px'}} option={options}/>
+
+      </Card>
+
+      
+
     </div>
   );
 }
